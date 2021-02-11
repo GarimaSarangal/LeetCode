966e82854d8931a7b74ca4f9ed95f96ae098f7a6
Garima Sarangal <37832421+GarimaSarangal@users.noreply.github.com>
2021-02-11 11:54:31 -0600

    RotateImage

--- RotateImage.py
+++ RotateImage.py
@@ -4,21 +4,26 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         #https://leetcode.com/problems/rotate-image/solution/
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
 #Solution 1
-        # for rotation matrix reverse and then transpose
-        # reverse first
-        matrix.reverse()
-        n = len(matrix[0])
+     #reversed
+        n = len(matrix)
+        e = n-1
+        for i in range(n//2):
+            matrix[i], matrix[e] = matrix[e], matrix[i]
+            e -= 1
         
-        # transpose of matrix
-        for i in range(n):
-            for j in range(i,n):
+        #transpose 
+        for i in range(len(matrix[0])):
+            for j in range(i):
                 matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
-        
+
 #Solution 2
 # or do transpose and reverse
-       
-
 #         n = len(matrix[0])
 #         # transpose of matrix
 #         for i in range(n):
@@ -29,3 +34,17 @@ class Solution:
 #         # reverse a matrix
 #         for i in range(n):
 #             matrix[i].reverse()
+       
+                
+#Solution 3
+# for rotation matrix reverse and then transpose
+#         # reverse first
+#         matrix.reverse()
+#         n = len(matrix[0])
+        
+#         # transpose of matrix
+#         for i in range(n):
+#             for j in range(i,n):
+#                 matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+        
+            
