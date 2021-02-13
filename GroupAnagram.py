f3644fa2f2694b071c22acd8a41f49e666bd14a1
Garima Sarangal <37832421+GarimaSarangal@users.noreply.github.com>
2021-02-12 19:58:23 -0600

    GroupAnagram

--- GroupAnagram.py
+++ GroupAnagram.py
@@ -1,7 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        
+        # https://leetcode.com/problems/group-anagrams/  
+        #TC - O(NKlogK) - where NN is the length of strs, and KK is the maximum length of a string in strs.
+        #O(NK) 
         ans  = collections.defaultdict(list)
         
         for s in strs:
