8a5fa5e5eabf49c430b60735be2524f4f89c0ebf
Garima Sarangal <37832421+GarimaSarangal@users.noreply.github.com>
2021-02-13 00:51:30 -0600

    SearchInRotatedSortedArray

--- Search_in_rotated_Array.py
+++ Search_in_rotated_Array.py
@@ -1,6 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
     #https://leetcode.com/problems/search-in-rotated-sorted-array/
+    #TC -  O(logn)
+    #SC - O(1)
         low, high = 0 , len(nums)-1
         if len(nums) == 1 and target == nums[0]:
             return 0
