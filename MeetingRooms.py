664d831919b671cad45fe0cba7e4f8f0960a8927
Garima Sarangal <37832421+GarimaSarangal@users.noreply.github.com>
2021-02-14 18:22:44 -0600

    MeetingRooms

---
+++ MeetingRooms.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+   
+    #TC - nlogn
+    #Sc - O(1)
+        
+        intervals.sort(key=lambda x:x[0])
+        for i in range(len(intervals)-1):
+            if intervals[i+1][0] < intervals[i][1]:
+                return False
+            else:
+                intervals[i+1][1] = max(intervals[i][1], intervals[i+1][1])
+        
+        return True
+        
