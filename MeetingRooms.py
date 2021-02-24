f051d01d68c4364dad1db189ea0ea7ef225e1e49
Garima Sarangal <37832421+GarimaSarangal@users.noreply.github.com>
2021-02-24 10:52:31 -0600

    Meeting Rooms

--- MeetingRooms.py
+++ MeetingRooms.py
@@ -13,3 +13,10 @@ class Solution:
         
         return True
         
+#SOlution 2 - EASIERRR
+        intervals.sort()
+        for i in range(1,len(intervals)):
+            if intervals[i][0] < intervals[i-1][1]:
+                return False
+            
+        return True
