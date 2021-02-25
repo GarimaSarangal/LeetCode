f2c1f2164e7134b14a89bba7024e211fc4d9408d
Garima Sarangal <37832421+GarimaSarangal@users.noreply.github.com>
2021-02-25 12:57:49 -0600

    Updated

--- MedianOfTwoSortedArrays.py
+++ MedianOfTwoSortedArrays.py
@@ -1,37 +1,37 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+            
         lNums1 = len(nums1)
         lNums2 = len(nums2)
         
-        if lNums1 > lNums2:
-            self.findMedianSortedArrays(nums2, nums1)
         
         low , high = 0, lNums1
         
-        while low < high:
+        while low <= high:
+
             partNums1 = (low+high)//2
+            
             partNums2 = ((lNums1 + lNums2 + 1) // 2) - partNums1
-        
+            
+
             max1 = -math.inf if partNums1 == 0 else nums1[partNums1-1]
             min1 = math.inf if partNums1 == lNums1 else nums1[partNums1]
-        
+
             max2 = -math.inf if partNums2 == 0 else nums2[partNums2-1]
             min2 = math.inf if partNums2 == lNums2 else nums2[partNums2]
         
-            if max1<=min2 and max2<=min1:
+            if max1 <= min2 and max2 <= min1:
                 if (lNums1+lNums2) % 2 == 0:
-                    return max(max1,max2)+min(min1.min2)/2
+                    return (max(max1,max2) + min(min1,min2))/2
                 else:
                     return max(max1,max2)
-            elif max1 > min2:
-                high = partNums1-1
-            else:
-                low = partNums1+1
-       
-    
-        
-        
-        
-        
                 
+            elif max1 > min2:
+                high = partNums1 - 1
+            else:
+                low = partNums1 + 1
+                
+        raise ValueError("Arrays must be sorted")
